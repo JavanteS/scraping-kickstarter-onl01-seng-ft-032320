f22cc95801bedeb767d6e7511c476103d3fabfa0
Javante Nelson <javantenelson@gmail.com>
2020-04-09 14:18:53 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,5 +11,5 @@ def create_project_hash
 end
 #kickstarter.css("li.project.grid_4").first
 #project.css("h2.bbcard_name strong a").text
-#
+#project.css("div.project-thumbnail a img").attribute("src").value
 create_project_hash
